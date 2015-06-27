a732fa6ae9c404506b136402831ee9968f389285
Diego Nieto Cid <dnietoc@gmail.com>
2015-06-27 00:50:05 -0300

    Se corre el test del servidor de ids con 'make check'

--- tests/id-server/main.cpp
+++ tests/id-server/main.cpp
@@ -1,20 +1,33 @@
 #include <id-server/IIdClient.h>
 #include <iostream>
+#include <libgen.h>
 #include <signal.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <utils/System.h>
 #include <vector>
 
+std::string calcularRutaServidor (const char* arg0)
+{
+	char* tmp = strdup (arg0);
+	char* dir = dirname (tmp);
+	std::string ruta = dir;
+	ruta.append("/../../src/id-server/id-server");
+	free (tmp);
+	return ruta;
+}
+
 int main (int argc, char** argv)
 {
 	pid_t srv_pid;
 	std::vector<const char*> srv_args;
-	const char* srv_bin = "../../src/id-server/id-server";
-	srv_args.push_back (srv_bin);
+	std::string srv_bin = calcularRutaServidor (argv[0]);
+	srv_args.push_back (srv_bin.c_str ());
 	srv_args.push_back ("1001");
 	srv_args.push_back (NULL);
-	srv_pid = System::spawn (srv_bin, srv_args);
+	srv_pid = System::spawn (srv_bin.c_str (), srv_args);
 	System::check (srv_pid);
 
 	// Espera "un rato" para que el server inicialice
