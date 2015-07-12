82f63ee7f6dcf748163cddf8ed658cf963616207
Diego Nieto Cid <dnietoc@gmail.com>
2015-07-11 22:04:26 -0300

    ipersona_comp_lector: manejar señales.

--- src/ipersona_comp/ipersona_comp_lector.cpp
+++ src/ipersona_comp/ipersona_comp_lector.cpp
@@ -1,5 +1,7 @@
 #include "ArgParserLector.h"
 #include "ipc-keys.h"
+#include <IPC/SIGINT_Handler.h>
+#include <IPC/SignalHandler.h>
 #include "IPCManager.h"
 #include <ipersona/IPersonaMsg.h>
 
@@ -15,16 +17,26 @@ int main (int argc, char** argv)
 	ArgParserLector& args = ArgParserLector::getInstance ();
 	args.parse (argc, argv);
 
+	SIGINT_Handler intHandler;
+	SignalHandler* sigs = SignalHandler::getInstance ();
+	sigs->registrarHandler (SIGINT, &intHandler);
+
 	std::string pathColas = calcularPathColas (args);
 	IPCManager ipcman (args.idLocal (), args.mqInterfaz (), pathColas);
 
 	// TODO: crear socket hacia el broker...
 	// TODO: enviar id de puerta al broker
 
-	while (1) {
-		// TODO: leer paquete del broker
-		// TODO: convertir paquete a IPersonaMsg
-		IPersonaMsg msg;
-		// TODO: colocar paquete en cola adecuada
+	while (intHandler.getGracefulQuit () == 0) {
+		try {
+			// TODO: leer paquete del broker
+			// TODO: convertir paquete a IPersonaMsg
+			IPersonaMsg msg;
+			// TODO: colocar paquete en cola adecuada
+		} catch (std::exception& e) {
+			// TODO: log, eintr, etc
+		}
 	}
+
+	sigs->destruir ();
 }
